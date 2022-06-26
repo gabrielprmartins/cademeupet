eed01497a270381133c3b71721bd031e330c61b6
pamelarocha <gabrielpr.martins@gmail.com>
2022-06-25 22:18:33 -0300

    FormLogin

--- src/Components/Login/FormLogin.js
+++ src/Components/Login/FormLogin.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { TOKEN_POST } from '../../api';
+import useForm from '../../Hooks/useForm';
 import Button from '../FormComponents/Button';
 import Input from '../FormComponents/Input';
 import styles from './FormLogin.module.css';
 
 const FormLogin = () => {
   const navigate = useNavigate();
+  const username = useForm('email');
+  const password = useForm('');
+
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+      const response = await fetch(url, options);
+      console.log(response);
+      const json = await response.json();
+      console.log(json);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <section className={styles.formLogin}>
       <h1 className="title">Login</h1>
-      <form>
-        <Input label="E-mail" type="email" name="email" />
-        <Input label="Senha" type="password" name="password" />
+      <form onSubmit={handleSubmit}>
+        <Input label="E-mail" type="email" name="email" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Login</Button>
       </form>
       <Link to="esqueceu" className={styles.lostPass}>
--- src/api.js
+++ src/api.js
@@ -1,5 +1,18 @@
 export const API_URL = 'https://cademeupetapi.000webhostapp.com/json';
 
+export function TOKEN_POST(body) {
+  return {
+    url: API_URL + '/jwt-auth/v1/token',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+
 export function USER_POST(body) {
   return {
     url: API_URL + '/api/user',
